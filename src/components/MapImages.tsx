06d2eee54fea049e385b0f3b6566ed0e135cb906
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-02 20:10:45 +0300

    images added

--- src/components/MapImages.tsx
+++ src/components/MapImages.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { motion } from "framer-motion";
+import { animationStart } from "../utils/animation";
 
-const MapImages = () => {
-  return <div>MapImages</div>;
-};
+function MapImages() {
+  return (
+    <div className="absolute w-full flex justify-between transform -translate-y-200px -z-1 overflow-hidden">
+      <motion.img
+        src="src/assets/map_1.jpeg"
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          delay: animationStart + 1.2,
+          type: "tween",
+          duration: 0.5,
+        }}
+        className="object-cover"
+      />
+      <motion.img
+        src="src/assets/map_2.jpeg"
+        initial={{ x: 100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          delay: animationStart + 1.2,
+          type: "tween",
+          duration: 0.5,
+        }}
+        className="object-cover"
+      />
+    </div>
+  );
+}
 
 export default MapImages;
